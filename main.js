93fb08386967b033a6453a077e18396768978320
Your Name <you@example.com>
2023-11-26 20:20:53 +0100

    26/11

---
+++ main.js
@@ -0,0 +1,9 @@
+let names = ["hamza",  "ahmed", "ahmed" , "asma" , "layla", "yassine" , "omar", "khaled", "kawtar","mo3tasiim" , "ahlam"]
+
+
+names.forEach(( element , index )=>{
+    document.write(element + ' ' + index , "<br>")
+})
+
+
+
